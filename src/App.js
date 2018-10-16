7fd306ee4249ce715a3cef5a1bb4d5f7d3750590
雷鹏程 <v_leipc@fosun.com>
2018-10-16 11:07:59 +0800

    add code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    // getInitialState() {
+    //     return {
+    //         styles: {
+    //             fontSize: '40px',
+    //             color:'red',
+    //         }
+
+    //     }
+        
+    // };
+    counstructor(props) {
+        this.state = {
+            fontSize:'40px'
+        }
+    }
+
+    componentWillMount() {
+        console.log(1111)
+    }
+
+    componentWillMount() {
+        console.log(222)
+    }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
+          <p className='item'>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           {/* <a
